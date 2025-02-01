6b6071942ebcfb3a0c125b58fa0eb36569bc3e24
David Aerne <daerne@yahoo.com>
2025-01-31 19:31:32 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import SpellDatabase from './components/SpellDatabase'
+import React from 'react';
+import SpellDatabase from './components/SpellDatabase';
 
 function App() {
-    return (
-        <>
-            <SpellDatabase />
-        </>
-    )
+  return (
+    <div className="bg-gray-100 min-h-screen">
+      <SpellDatabase />
+    </div>
+  );
 }
 
-export default App
+export default App;
